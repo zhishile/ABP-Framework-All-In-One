29fb898c8f34cf0b1f497ec747fdfbc64ef770b4
51mono <46481080+zhishile@users.noreply.github.com>
2022-11-10 12:05:35 +0800

    框架升级：从 5.3.1 升级到 6.0.1

---
+++ src/AbpClub.EntityFrameworkCore/Migrations/20221109144458_UpdateSix.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbpClub.Migrations
+{
+    public partial class UpdateSix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "CmsUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "CmsUsers");
+        }
+    }
+}
